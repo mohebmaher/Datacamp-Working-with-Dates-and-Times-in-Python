f88c3e9855815b56ce3d15f20fab47ba0bae4b9d
moheb_maher <dvm.moheb.maher@gmail.com>
2022-10-24 18:40:16 +0200

    add chapter #2 - work in progress

--- assets.py
+++ assets.py
@@ -1,4 +1,4 @@
-# Unpack florida_hurricane_dates
+# Unpack florida_hurricane_dates dataset
 import pickle
 
 with open("./data/florida_hurricane_dates.pkl", 'rb') as f:
@@ -11,3 +11,10 @@ from random import shuffle, seed
 seed(42)
 dates_scrambled = florida_hurricane_dates.copy()
 shuffle(dates_scrambled)
+
+
+# Unpack onebike_datetimes list
+import pickle
+
+with open("./data/onebike_datetimes.pkl", 'rb') as f:
+    onebike_datetimes = pickle.load(f)
